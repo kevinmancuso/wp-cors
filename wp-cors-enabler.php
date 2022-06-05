f208dc2c381c113bf6faf0e071dde3247cf61174
kevinmancuso <44978649+kevinmancuso@users.noreply.github.com>
2022-06-04 20:02:43 -0400

    Adjust Spacing, Add Comments

--- wp-cors-enabler.php
+++ wp-cors-enabler.php
@@ -1,13 +1,15 @@
 function initCors( $value ) {
-    $origin = get_http_origin();
-    $allowed_origins = [ 'site1.example.com', 'site2.example.com', 'localhost:3000' ];
-    if ( $origin && in_array( $origin, $allowed_origins ) ) {
-      header( 'Access-Control-Allow-Origin: ' . esc_url_raw( $origin ) );
-      header( 'Access-Control-Allow-Methods: GET' );
-      header( 'Access-Control-Allow-Credentials: true' );
-    }
-    return $value;
+	$origin = get_http_origin();
+	// Add Urls to allow.
+	$allowed_origins = [ 'test1.com', 'test2.com', 'localhost:3000' ];
+	if ( $origin && in_array( $origin, $allowed_origins ) ) {
+		header( 'Access-Control-Allow-Origin: ' . esc_url_raw( $origin ) );
+		header( 'Access-Control-Allow-Methods: GET' );
+		header( 'Access-Control-Allow-Credentials: true' );
+	}
+	return $value;
 }
+
 // InitCors
 add_action( 'rest_api_init', function() {
 	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
